682bcf7543156701b84ba36ed55be68ffea3a479
martinezchen <martinezchen01@gmail.com>
2023-05-13 14:39:42 +0800

    feature cashflow
    建立訂單資料存入DB

--- src/main/java/com/wasupstudio/service/Impl/OrderItemServiceImpl.java
+++ src/main/java/com/wasupstudio/service/Impl/OrderItemServiceImpl.java
@@ -3,7 +3,9 @@ package com.wasupstudio.service.Impl;
 import com.wasupstudio.model.entity.OrderItemEntity;
 import com.wasupstudio.service.AbstractService;
 import com.wasupstudio.service.OrderItemService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderItemServiceImpl extends AbstractService<OrderItemEntity> implements
     OrderItemService {
 
